d1aa220eaeb6b65e7d8e5ef99012d21da0e0cef4
Johannes Deivard <jdev@DESKTOP-FM0NC9M.SWESYS.local>
2024-06-11 16:32:55 +0200

    Added buffer shortcut

--- init.lua
+++ init.lua
@@ -442,6 +442,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', '<cmd>Telescope buffers<cr><esc>', { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<C-space>', '<cmd>Telescope buffers<cr><esc>', { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
